3b8c1ccc2d503edaa2fa56a63269dcfc2af6152a
inuit57 <inuit57@naver.com>
2018-05-24 20:31:49 +0900

    Create motor.h

---
+++ plant/motor.h
@@ -0,0 +1,19 @@
+#ifndef __MOTOR_H__
+#define __MOTOR_H__
+
+#include<wiringPi.h>
+#include<stdio.h>
+#include<softPwm.h>
+
+#define SERVO_PIN 1 //GPIO 18 
+
+#define CENTER 15 //center.
+#define RIGHT 24 //right 90
+#define LEFT 8 //left 90
+#define OTHERSIDE 2 //180도 회전
+
+int initMotor(void);
+int getMotorDir(void); 
+void MotorControl(int dir); 
+
+#endif /* __MOTOR_H__ */ 
