3c04ee4a3387f7467953adbb136b4bc891f2aa3d
inuit57 <inuit57@naver.com>
2018-05-24 20:46:23 +0900

    Create sensor.h

---
+++ plant/sensor.h
@@ -0,0 +1,15 @@
+#ifdef __SENSOR_H__
+#define __SENSOR_H__
+
+#include<stdio.h>
+#include<WiringPi.h>
+#include<pigpiod_if2.h>
+
+#define BAUD_RATE 1000000
+#define LOOP 1000
+
+int init_sensor(void);
+int getHumidity(unsigned char channel);
+
+
+#endif /* __SENSOR_H__
