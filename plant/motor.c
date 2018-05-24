f37e9a4f26a8c952368cecac26685ca7d7df9c5b
inuit57 <inuit57@naver.com>
2018-05-24 20:37:45 +0900

    Update and rename servo.c to motor.c

---
+++ plant/motor.c
@@ -0,0 +1,28 @@
+#include"motor.h"                                                         
+
+int dir = CENTER ;
+int initMotor(void){
+	//if(wiringPiSetup() == -1) return 1;                                         
+	softPwmCreate(SERVO_PIN, 0, 200);  
+	return 0; 
+}
+
+int getMotorDir(void){
+	return dir;
+}
+	
+void MotorControl(int dir){
+		switch(dir){                                                            
+			case 'CENTER' : softPwmWrite(SERVO,15);                               
+					   break;                                                   
+			case 'RIGHT' : softPwmWrite(SERVO,24);                                 
+					   break;                                                                                               
+                               
+			case 'LEFT' : softPwmWrite(SERVO, 8);                                  
+					   break; //left 90                                                                         
+			case 'OTHERSIDE' : softPwmWrite(SERVO, 2);                                  
+					   break; //180                                                                                         
+			default : softPwmWrite(SERVO,15);                                   
+					  break;                                                    
+		}                                                                                                                                                                                                                
+}     
